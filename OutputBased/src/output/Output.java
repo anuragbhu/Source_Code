98016cf65d29dbbf53416ac140adaf6190cdab90
anuragbhu <anurag.bhumca18@gmail.com>
2021-09-06 14:46:28 +0530

    Added new problems

---
+++ OutputBased/src/output/Output.java
@@ -0,0 +1,15 @@
+package output;
+
+public class Output {
+
+	public static void main(String[] args) {
+		
+		int a = 10, b = 20;
+		
+		System.out.println(a+++--b);
+//		System.out.println(a--+++b);
+		System.out.println(a++-++b);
+		System.out.println(a+++(++b));
+	}
+
+}
