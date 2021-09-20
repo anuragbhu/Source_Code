050930c9b53c1a8869b9055109c87c5c8694143c
anuragbhu <anurag.bhumca18@gmail.com>
2021-09-20 19:52:31 +0530

    Commit

--- Arrays/src/NegativeElementsToOneSide.java
+++ Arrays/src/NegativeElementsToOneSide.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+
 public class NegativeElementsToOneSide {
 
 	public static void main(String[] args) {
@@ -14,8 +15,9 @@ public class NegativeElementsToOneSide {
 //		Order of elements is not important here.
 //		rearrange(arr, n); // TC = O(nlogn), SC = O(1) for array having n elements --- Brute Force Approach
 //		rearrangeOptimal(arr, n); //  TC = O(n), SC = O(1) ---  in-place rearranging algorithm --- Two Pointer Approach/Algorithm
-		rearrangeSimple(arr, n); //  TC = O(n), SC = O(1) ---  in-place rearranging algorithm --- Two Pointer Approach/Algorithm
-		
+//		rearrangeSimple(arr, n); //  TC = O(n), SC = O(1) ---  in-place rearranging algorithm --- Two Pointer Approach/Algorithm
+		rearrangeOrderMatters(arr, n);
+		System.out.println("Output");
 		printArray(arr, n);
 		sc.close();
 
@@ -65,6 +67,36 @@ public class NegativeElementsToOneSide {
 	
 	public static void rearrange(int[] arr, int n) {
 		Arrays.sort(arr);
+	}
+	
+	// Order Matters --- Not Possible in TC = O(n) and SC = O(1)
+	// https://stackoverflow.com/questions/4897280/given-an-array-of-positive-and-negative-integers-re-arrange-it-so-that-you-have
+	public static void rearrangeOrderMatters(int[] arr, int n) {
+		
+		int positive[] = new int[n];
+		int negative[] = new int[n];
+		
+		int neg = 0, pos = 0;
+		
+		for(int i = 0; i < n; i++) {
+			if(arr[i] < 0) {
+				negative[neg] = arr[i];
+				neg++;
+			} else {
+				positive[pos] = arr[i];
+				pos++;
+			}
+		}
+		
+		int i = 0;
+		
+		for(int j = 0; i < n && j <= neg; i++, j++) {
+			arr[i] = negative[j];
+		}
+		i--;
+		for(int j = 0; i < n && j <= pos; i++, j++) {
+			arr[i] = positive[j];
+		}
 	}
 	
 	public static void printArray(int[] arr, int n) {
--- Test/src/Test.java
+++ Test/src/Test.java
@@ -29,79 +29,19 @@ public class Test {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		int n = sc.nextInt();
-		int check = Integer.MAX_VALUE;
-		boolean  flag = true;
+		int a = 60, b = 80;
 		
-		for(int i = 1; i <= n; i++) {
-			int temp =  sc.nextInt();
-			if(temp < check) {
-				if(!flag) {
-					System.out.println(false);
-					return;
-				}
-			} else if(temp > check) {
-				flag = false;
-			} else {
-				System.out.println(false);
-				return;
-			}
-			check = temp;
+		if(a == 60 || b++ > 80) {
+			System.out.println("Inside IF");
+		} else {
+			System.out.println("Inside ELSE");
 		}
 		
-		System.out.println(true);
-		
-		
-		
-//		pattern(n);
-
-//		int prod = 1;
-//		
-//		for(int i = 2; i<= n; i++) {
-//			prod *= i;
-//		}
-//		
-//		System.out.println(prod);
+		System.out.println("a: " + a + " b: " + b);
 		
 		sc.close();
 	
 		
-	}
-	
-	
-	public static void pattern(int n) {
-		
-		for(int i = 1; i <= n; i++) {
-			for(int j = 1; j <= i; j++) {
-				System.out.print(" ");
-			}
-			
-			for(int j = i; j <= n; j++) {
-				System.out.print("* ");
-			}
-			
-			for(int j = 1; j <= i; j++) {
-				System.out.print(" ");
-			}
-			
-			System.out.println();
-		}
-		
-		for(int i = 1; i <= n; i++) {
-			for(int j = i; j <= n; j++) {
-				System.out.print(" ");
-			}
-			
-			for(int j = 1; j <= i; j++) {
-				System.out.print("* ");
-			}
-			
-			for(int j = i; j <= n; j++) {
-				System.out.print(" ");
-			}
-			
-			System.out.println();
-		}
 	}
 
 }
